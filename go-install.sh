de3274df8207999e9deeed492c2e296b5286c7ca
pandrew <letters@paulnotcom.se>
2014-06-12 23:06:27 +0200

    remove vet

--- go-install.sh
+++ go-install.sh
@@ -7,4 +7,3 @@ go get code.google.com/p/rog-go/exp/cmd/godef
 go get code.google.com/p/go.tools/cmd/goimports
 go get github.com/monochromegane/the_platinum_searcher 
 go get github.com/golang/lint/golint
-go get code.google.com/p/go.tools/cmd/vet
