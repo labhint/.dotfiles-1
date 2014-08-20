882037b21892d9af2749b51935690451734f08ef
Paul Andrew Liljenberg <letters@paulnotcom.se>
2014-08-20 18:57:44 +0200

    update uri for platinum_searcher

--- go-install.sh
+++ go-install.sh
@@ -4,6 +4,6 @@ go get github.com/rakyll/boom
 go get github.com/jstemmer/gotags
 go get code.google.com/p/rog-go/exp/cmd/godef
 go get code.google.com/p/go.tools/cmd/goimports
-go get github.com/monochromegane/the_platinum_searcher 
+go get -u github.com/monochromegane/the_platinum_searcher/...
 go get github.com/golang/lint/golint
 go get github.com/dotcloud/gordon/{pulls,issues}
